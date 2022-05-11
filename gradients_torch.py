4f7073ab4c353b22ef47395e9c23ec2fa03f90ce
skim658 <33474168+seankim658@users.noreply.github.com>
2022-05-10 23:50:10 -0400

    finishing linear regression

--- gradients_torch.py
+++ gradients_torch.py
@@ -1,41 +1,51 @@
-# linear regression with gradint descent using autograd 
+# linear regression with pytorch
 
+# 1) Design model (input, output size, forward pass)
+# 2) Construct loss and optimizer 
+# 3) Training loop
+#   - forward pass: compute prediciton 
+#   - backward pass: gradients 
+#   - update weights 
+
+from pickletools import optimize
+from numpy import float32
 import torch
+import torch.nn as nn 
 
 # f = w * x: f = 2 * x 
-x = torch.tensor([1, 2, 3, 4], dtype = torch.float32)
-y = torch.tensor([2, 4, 6, 8], dtype = torch.float32)
+x = torch.tensor([[1], [2], [3], [4]], dtype = torch.float32)
+y = torch.tensor([[2], [4], [6], [8]], dtype = torch.float32)
 
-# initial weight
-w = torch.tensor(0.0, dtype = torch.float32, requires_grad = True)
+n_samples, n_features = x.shape
+input_size = n_features
+output_size = n_features
 
-# model prediction 
-def forward_pass(x):
-    return w * x 
+model = nn.Linear(input_size, output_size)
 
-# loss: MSE
-def loss_fn(y, y_pred):
-    return ((y_pred - y)**2).mean()
-
-print(f'Initial predictions: f(5) = {forward_pass(5):.3f}')
+print(f'Initial predictions: f(5) = {model(torch.tensor([5], dtype = torch.float32)).item():.3f}')
 
 # training
 learning_rate = 0.01
-n_iters = 100
+n_iters = 150
+
+loss = nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
 
 for epoch in range(n_iters):
     # prediction = forward pass
-    y_preds = forward_pass(x)
+    y_preds = model(x)
     # loss
-    loss = loss_fn(y, y_preds)
+    l = loss(y, y_preds)
     # gradients = backward pass 
-    loss.backward() # dl/dw 
+    l.backward() # dl/dw 
     # update weight 
-    with torch.no_grad():
-        w -= learning_rate * w.grad
+    optimizer.step()
     # zero gradients 
-    w.grad.zero_()
+    optimizer.zero_grad()
     if epoch % 10 == 0:
-        print(f'epoch {epoch + 1}: w = {w:.3f}, loss = {loss:.4f}')
+        print(f'epoch {epoch + 1}: w = {model.weight.item():.3f}, loss = {l:.4f}')
+        # for param in model.parameters():
+        #     print(param)
+        print('---------------------------')
 
-print(f'Final prediction: f(5) = {forward_pass(5):.3f}')
+print(f'Final prediction: f(5) = {model(torch.tensor([5], dtype = torch.float32)).item():.3f}')
