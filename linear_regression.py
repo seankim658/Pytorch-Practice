4553b6652c3d30bbb720b784ba16c121c2b6b3dd
skim658 <33474168+seankim658@users.noreply.github.com>
2022-05-12 23:50:08 -0400

    linear regression pt II

--- linear_regression.py
+++ linear_regression.py
@@ -3,3 +3,49 @@ import torch.nn as nn
 import numpy as np
 from sklearn import datasets 
 import matplotlib.pyplot as plt 
+
+
+# 0) prepare data 
+x_numpy, y_numpy = datasets.make_regression(n_samples = 100, n_features = 1, noise = 20, random_state = 1)
+
+x = torch.from_numpy(x_numpy.astype(np.float32))
+y = torch.from_numpy(y_numpy.astype(np.float32))
+y = y.view(y.shape[0], 1)
+
+n_samples, n_features = x.shape 
+
+# 1) model
+input_size = n_features
+output_size = 1
+
+model = nn.Linear(input_size, output_size)
+
+# 2) loss and optimizer
+learning_rate = 0.01
+criterion = nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
+
+# 3) training loop 
+epochs = 100
+for epoch in range(epochs):
+    # forward pass
+    y_pred = model(x)
+    loss = criterion(y_pred, y)
+
+    # backward pass
+    loss.backward()
+
+    # update 
+    optimizer.step()
+
+    # empty gradients 
+    optimizer.zero_grad()
+
+    if (epoch + 1) % 10 == 0:
+        print(f'epoch: {epoch + 1}, loss = {loss.item():.4f}')
+
+# plot 
+predicted = model(x).detach().numpy()
+plt.plot(x_numpy, y_numpy, 'ro')
+plt.plot(x_numpy, predicted, 'b')
+plt.show()
