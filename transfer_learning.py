00a2f98c56f4423604f00729f4328022ea7e0c6f
skim658 <33474168+seankim658@users.noreply.github.com>
2022-05-25 14:02:41 -0400

    starting transfer learning

---
+++ transfer_learning.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim 
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision 
+from torchvision import datasets, models, transforms 
+import matplotlib.pyplot as plt
+import time 
+import os 
+import copy 
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
