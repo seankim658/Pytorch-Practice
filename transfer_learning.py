7d98bb3695e01faea3e9e73cb12208bf6a3af8a1
skim658 <33474168+seankim658@users.noreply.github.com>
2022-05-29 00:53:24 -0400

    still working on transfer learning

--- transfer_learning.py
+++ transfer_learning.py
@@ -33,7 +33,7 @@ data_transforms = {
 
 data_root = 'data/hymenoptera_data'
 image_datasets = {x: datasets.ImageFolder(os.path.join(data_root, x), data_transforms[x]) for x in ['train', 'val']}
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size = 4, shuffle = True) for x in ['train', 'val']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size = 4, shuffle = False) for x in ['train', 'val']}
 
 dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
 class_names = image_datasets['train'].classes
@@ -43,6 +43,7 @@ def plot_images(input, title):
     # transpose image so it is [height x width x color channels]
     input = input.numpy().transpose(1, 2, 0)
     input = std * input + mean
+    input = np.clip(input, 0, 1)
     plt.imshow(input)
     plt.title(title)
     plt.show()
@@ -50,3 +51,26 @@ def plot_images(input, title):
 images, classes = next(iter(dataloaders['train']))
 out = torchvision.utils.make_grid(images)
 plot_images(out, title = [class_names[x] for x in classes])
+
+def train(model, criterion, optimizer, scheduler, epochs = 25):
+    since = time.time()
+
+    best_model_weights = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(epochs):
+        print(f'Epoch {epoch}/{epochs - 1}')
+        print('---------')
+
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+            
+            loss = 0.0
+            correct = 0
+
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
